bc3c2a49c31cafd51b447247936da0caad553b67
LipehSantos213 <fl9558125@gmail.com>
2026-06-08 15:50:17 -0300

    refactor: improvement in consultations and records

--- Infrastructure/Repositories/TodoTaskRepository.cs
+++ Infrastructure/Repositories/TodoTaskRepository.cs
@@ -1,6 +1,7 @@
 ﻿using api_csharp.Domain.Entities;
 using api_csharp.Domain.Interfaces;
 using api_csharp.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace api_csharp.Infrastructure.Repositories
 {
@@ -15,9 +16,42 @@ namespace api_csharp.Infrastructure.Repositories
 
         public async Task<TodoTask> Create(TodoTask task)
         {
-            _context.TodoTasks.Add(task);
+            _context.TodoTasks.Where(i => i.UserId == task.UserId).Append(task);
             await _context.SaveChangesAsync();
             return task;
+        }
+    
+        public async Task<List<TodoTask>?> GetAll(int userId)
+        {
+            List<TodoTask>? tasks = await _context.TodoTasks
+                .Where(i=> i.UserId == userId).ToListAsync();
+
+            return tasks;
+        }
+    
+        public async Task<TodoTask> Update(TodoTask task)
+        {
+            await _context.TodoTasks.Where(i => i.UserId == task.UserId)
+                .ExecuteUpdateAsync(s=> s
+                .SetProperty(t=> t.Title, task.Title)
+                .SetProperty(t=> t.Description, task.Description)
+                );
+            //_context.TodoTasks.Update(task);
+            //await _context.SaveChangesAsync();
+            return task;
+        }
+
+        public async Task<TodoTask?> Get(int userId, int id)
+        {
+            TodoTask? task = await _context.TodoTasks.FirstOrDefaultAsync(i=> i.Id == id && i.UserId == userId);
+            return task;
+        }
+    
+        public async Task Remove(int userId, int id)
+        {
+            await _context.TodoTasks
+                .Where(i => i.UserId == userId && i.Id == id)
+                .ExecuteDeleteAsync();
         }
     }
 }
--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -21,13 +21,13 @@ namespace api_csharp.Infrastructure.Repositories
             return user;
         }
 
-        public async Task<User> Get(int id)
+        public async Task<User?> Get(int id)
         {
             // Busca o usuário pelo ID, ou retorna null se não achar
             return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
         }
 
-        public async Task<User> Login(User user)
+        public async Task<User?> Login(User user)
         {
             // Busca o usuário que bate com o e-mail informado
             // Nota: Na vida real, você buscaria pelo e-mail e validaria o Hash da senha.
@@ -37,8 +37,21 @@ namespace api_csharp.Infrastructure.Repositories
 
         public async Task<User> Update(User user)
         {
-            _context.Users.Update(user);
-            await _context.SaveChangesAsync();
+            await _context.Users.Where(u=> u.Id == user.Id).ExecuteUpdateAsync(s => s
+            .SetProperty(u => u.Name, user.Name)
+            .SetProperty(u=> u.Email, user.Email)
+            );
+            //_context.Users.Update(user);
+            //await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User?> GetUserByEmail(string email)
+        {
+            var user = await _context.Users
+                .Where(u => u.Email.Address == email)
+                .FirstOrDefaultAsync();
+
             return user;
         }
     }
