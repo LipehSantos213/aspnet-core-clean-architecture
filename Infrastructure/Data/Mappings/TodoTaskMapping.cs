e0b7ce4a26b82200ae79c1312611d0613080d10c
LipehSantos213 <fl9558125@gmail.com>
2026-06-08 15:38:35 -0300

    feat: mapping todo tasks and users

---
+++ Infrastructure/Data/Mappings/TodoTaskMapping.cs
@@ -0,0 +1,35 @@
+﻿using api_csharp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace api_csharp.Infrastructure.Data.Mappings
+{
+    public class TodoTaskMapping : IEntityTypeConfiguration<TodoTask>
+    {
+        public void Configure(EntityTypeBuilder<TodoTask> builder)
+        {
+            // Define o nome da tabela no banco
+            builder.ToTable("TodoTask");
+
+            // Define o 'Id' como chave primaria
+            builder.HasKey(t => t.Id);
+
+            // Configurando a propriedade 'Title'
+            builder.Property(t => t.Title)
+                .IsRequired() // NotNull
+                .HasMaxLength(150); // --> VarChar(150)
+
+            builder.Property(t => t.Description)
+                .HasMaxLength(500);
+
+            builder.Property(t => t.Active)
+                .HasDefaultValue(true);
+
+            // Configurando a Chave Estrangeira (Relacionamento)
+            builder.HasOne<User>() // Uma tarefa tem um Usuário
+                .WithMany()        // Um usuário tem muitas tarefas
+                .HasForeignKey(t => t.UserId) // Chave estrangeira na tabela TodoTasks
+                .OnDelete(DeleteBehavior.Cascade); // Se apagar o usuário, apaga as tarefas dele
+        }
+    }
+}
---
+++ Infrastructure/Data/Mappings/UserMapping.cs
@@ -0,0 +1,34 @@
+﻿using api_csharp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace api_csharp.Infrastructure.Data.Mappings
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            // Define o nome da tabela no PostgreSQL
+            builder.ToTable("users");
+
+            // Define o 'Id' como chave primaria
+            builder.HasKey(u => u.Id);
+
+            // Configuração da propriedade (coluna) 'Name' no banco
+            builder.Property(u => u.Name)
+                .IsRequired() // NotNull
+                .HasMaxLength(100); // --> VarChar(100)
+
+            builder.OwnsOne(u => u.Email, email =>
+            {
+                email.Property(e => e.Address)
+                    .HasColumnName("Email") // Nome da coluna no banco
+                    .IsRequired()
+                    .HasMaxLength(150);
+            });
+
+            // Configuração para o EF conseguir ler propriedades com "private set"
+            builder.Navigation(u => u.Email).IsRequired();
+        }
+    }
+}
