bd0bc618f9894086d3dd6fa548114ec1f62dab37
LipehSantos213 <fl9558125@gmail.com>
2026-06-08 15:51:07 -0300

    refactor: improvement in consultations and records

--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -29,8 +29,6 @@ namespace api_csharp.Infrastructure.Repositories
 
         public async Task<User?> Login(User user)
         {
-            // Busca o usuário que bate com o e-mail informado
-            // Nota: Na vida real, você buscaria pelo e-mail e validaria o Hash da senha.
             return await _context.Users
                 .FirstOrDefaultAsync(u => u.Email.Address == user.Email.Address);
         }
@@ -41,8 +39,6 @@ namespace api_csharp.Infrastructure.Repositories
             .SetProperty(u => u.Name, user.Name)
             .SetProperty(u=> u.Email, user.Email)
             );
-            //_context.Users.Update(user);
-            //await _context.SaveChangesAsync();
             return user;
         }
 
