a83c553356cf840f4e6ba0d22848470f60bd1bb6
LipehSantos213 <fl9558125@gmail.com>
2026-06-08 15:57:00 -0300

    feat: add routes of users

--- Presentation/Controllers/UsersController.cs
+++ Presentation/Controllers/UsersController.cs
@@ -10,11 +10,19 @@ namespace api_csharp.Presentation.Controllers
     {
         private readonly CreateUserUseCase _createUserUseCase;
         private readonly LoginUserUseCase _loginUserUseCase;
+        private readonly GetUserUseCase _getUserUseCase;
+        private readonly UpdateUserUseCase _updateUserUseCase;
 
-        public UsersController(CreateUserUseCase createUserUseCase, LoginUserUseCase loginUserUseCase)
+        public UsersController(
+            CreateUserUseCase createUserUseCase,
+            LoginUserUseCase loginUserUseCase,
+            GetUserUseCase getUserUseCase,
+            UpdateUserUseCase updateUserUseCase)
         {
             _createUserUseCase = createUserUseCase;
             _loginUserUseCase = loginUserUseCase;
+            _getUserUseCase = getUserUseCase;
+            _updateUserUseCase = updateUserUseCase;
         }
 
         [HttpPost] // Rota: POST api/users
@@ -22,9 +30,23 @@ namespace api_csharp.Presentation.Controllers
         {
                 var result = await _createUserUseCase.Execute(dto);
 
-                return CreatedAtAction(nameof(Create), new { id = result.Id }, result);
+                return CreatedAtAction(nameof(Get), new { id = result.Id }, result);
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            var user = await _getUserUseCase.Execute(id);
+            return Ok(user);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UserUpdateDTO dto)
+        {
+            var user = await _updateUserUseCase.Execute(dto);
+
+            return Ok(user);
+        }
 
         [HttpPost("login")] // Rota: POST api/users/login
         public async Task<IActionResult> Login([FromBody] UserLoginDTO dto)
