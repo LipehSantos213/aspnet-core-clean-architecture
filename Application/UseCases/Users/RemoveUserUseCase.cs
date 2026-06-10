a5e9f58ed0fa93c773b7c57770e2fb016c326212
LipehSantos213 <fl9558125@gmail.com>
2026-06-10 12:14:52 -0300

    feat: add user logout

---
+++ Application/UseCases/Users/RemoveUserUseCase.cs
@@ -0,0 +1,27 @@
+﻿using api_csharp.Application.Services;
+using api_csharp.Domain.Entities;
+using api_csharp.Domain.Interfaces;
+
+namespace api_csharp.Application.UseCases.Users
+{
+    public class RemoveUserUseCase
+    {
+        private readonly IUserRepository _repository;
+        private readonly UserService _userService;
+
+        public RemoveUserUseCase(
+            IUserRepository repository,
+            UserService userService)
+        {
+            _repository = repository;
+            _userService = userService;
+        }
+
+        public async Task<string> Execute(int userId)
+        {
+            User user = await _userService.AsyncGetUser(userId);
+
+            return await _repository.Remove(user);
+        }
+    }
+}
