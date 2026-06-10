6b854dcc98aeadb6ee0f827efdc18f2d29107c84
LipehSantos213 <fl9558125@gmail.com>
2026-06-10 12:11:08 -0300

    feat: add task services

---
+++ Application/Services/TodoTaskService.cs
@@ -0,0 +1,23 @@
+﻿using api_csharp.Domain.Entities;
+using api_csharp.Domain.Exception;
+using api_csharp.Domain.Interfaces;
+
+namespace api_csharp.Application.Services
+{
+    public class TodoTaskService
+    {
+        private readonly ITodoTaskRepository _repository;
+
+        public TodoTaskService(ITodoTaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<TodoTask> AsyncGetTodoTask(int userId, int id)
+        {
+            TodoTask task = await _repository.Get(userId, id) ??
+                throw new DomainException("Tarefa não encontrada !");
+            return task;
+        }
+    }
+}
